f6ed7bd2227d3dfab9be3df45de7693b64295f83
Katikala Vyshnavi <Katikala.Vyshnavi@gds.ey.com>
2021-03-04 19:28:50 +0530

    completed

---
+++ daytwelve/DeamonThreadDemo.java
@@ -0,0 +1,27 @@
+package daytwelve;
+
+public class DeamonThreadDemo {
+	public DeamonThreadDemo() {
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				while (true) {
+					System.out.println("child thread 1......");
+				}
+			}
+		}).start();
+	}
+
+	public static void main(String[] args) {
+		new DeamonThreadDemo();
+		System.out.println("main thread started......");
+		Thread.currentThread().setDaemon(true);
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		System.exit(1);
+	}
+}

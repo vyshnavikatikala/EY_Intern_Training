814833f49df81540df64e7048ec69fd79f2a40dc
Vyshnavi Katikala <vyshna1999@gmail.com>
2021-03-25 21:56:53 +0530

    completed

---
+++ day24/FrameDemo.java
@@ -0,0 +1,13 @@
+package day24;
+
+import java.awt.Frame;
+
+public class FrameDemo {
+	public static void main(String[] args) {
+		Frame frame = new Frame();
+		frame.setSize(400, 500);
+		frame.setVisible(true);
+
+		System.out.println("Frame object created.....");
+	}
+}
